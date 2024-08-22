904d06a6bbda36613c09cfeebac36589ac4a1e26
tsuy <tchow@tutamail.com>
2024-08-22 15:38:36 -0400

    draw a sphere

--- bin/ray.ml
+++ bin/ray.ml
@@ -10,12 +10,26 @@ module Ray = struct
     }
   [@@deriving sexp]
 
-  let position ray time = Point3.(ray.origin + scale ray.direction time)
+  let position ray time : Point3.t = Point3.(ray.origin + scale ray.direction time)
 
-  let ray_color ray =
-    let unit_direction = Point3.unit_vector ray.direction in
-    let a = 0.5 *. (unit_direction.g +. 1.) in
-    Color.(
-      scale { r = 1.; g = 1.; b = 1. } (1. -. a) + scale { r = 0.5; g = 0.7; b = 1. } a)
+  let hit_sphere (center : Point3.t) (radius : Float.t) (ray : t) : Bool.t =
+    let oc = Point3.(center - ray.origin) in
+    let a = Vec3.dot ray.direction ray.direction in
+    let b = 2. *. Vec3.dot oc ray.direction in
+    let c = Point3.dot oc oc -. (radius *. radius) in
+    let discriminant = (b *. b) -. (4. *. a *. c) in
+    Float.(discriminant >= 0.)
+  ;;
+
+  let ray_color ray : Color.t =
+    let center = Vec3.{ r = 0.; g = 0.; b = -1. } in
+    let radius = 0.5 in
+    if hit_sphere center radius ray
+    then Vec3.{ r = 1.; g = 0.; b = 0. }
+    else (
+      let unit_direction = Point3.unit_vector ray.direction in
+      let a = 0.5 *. (unit_direction.g +. 1.) in
+      Vec3.(
+        scale { r = 1.; g = 1.; b = 1. } (1. -. a) + scale { r = 0.5; g = 0.7; b = 1. } a))
   ;;
 end
